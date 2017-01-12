db94895be8838e0bba40568e3fb0af576c2c9971
StompyNZ <russell@codestorm.nz>
2017-01-12 23:48:32 +1300

    Removed unused code

--- BCManager/src/PersistentData/Players.cs
+++ BCManager/src/PersistentData/Players.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Text.RegularExpressions;
 
 namespace BCM.PersistentData
 {
@@ -35,66 +34,9 @@ namespace BCM.PersistentData
       }
     }
 
-    public List<string> SteamIDs
-    {
-      get { return new List<string>(players.Keys); }
-    }
-
     public int Count
     {
       get { return players.Count; }
-    }
-
-    //		public Player GetPlayerByNameOrId (string _nameOrId, bool _ignoreColorCodes)
-    //		{
-    //			string sid = GetSteamID (_nameOrId, _ignoreColorCodes);
-    //			if (sid != null)
-    //				return this [sid];
-    //			else
-    //				return null;
-    //		}
-
-    public string GetSteamID(string _nameOrId, bool _ignoreColorCodes)
-    {
-      if (_nameOrId == null || _nameOrId.Length == 0)
-      {
-        return null;
-      }
-
-      long tempLong;
-      if (_nameOrId.Length == 17 && long.TryParse(_nameOrId, out tempLong))
-      {
-        return _nameOrId;
-      }
-      else
-      {
-        int entityId = -1;
-        if (int.TryParse(_nameOrId, out entityId))
-        {
-          foreach (KeyValuePair<string, Player> kvp in players)
-          {
-            if (kvp.Value.IsOnline && kvp.Value.EntityID == entityId)
-            {
-              return kvp.Key;
-            }
-          }
-        }
-
-        _nameOrId = _nameOrId.ToLower();
-        foreach (KeyValuePair<string, Player> kvp in players)
-        {
-          string name = kvp.Value.Name.ToLower();
-          if (_ignoreColorCodes)
-          {
-            name = Regex.Replace(name, "\\[[0-9a-fA-F]{6}\\]", "");
-          }
-          if (kvp.Value.IsOnline && name.Equals(_nameOrId))
-          {
-            return kvp.Key;
-          }
-        }
-      }
-      return null;
     }
   }
 }
