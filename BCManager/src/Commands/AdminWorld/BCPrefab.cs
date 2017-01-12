04b8c1f89a73e13b8e0164c3abd9631856cfa326
StompyNZ <russell@codestorm.nz>
2017-01-12 23:54:56 +1300

    Additional Features and fixes

--- BCManager/src/Commands/AdminWorld/BCPrefab.cs
+++ BCManager/src/Commands/AdminWorld/BCPrefab.cs
@@ -12,12 +12,18 @@ namespace BCM.Commands
     {
       EntityPlayer sender = GameManager.Instance.World.Entities.dict[_senderInfo.RemoteClientInfo.entityId] as EntityPlayer;
       //ChunkProviderGenerateWorldRandom2 _chunkProvider = new ChunkProviderGenerateWorldRandom2(GameManager.Instance.World.ChunkCache, GameManager.Instance.World.GetWorldCreationData(), GamePrefs.GetString(EnumGamePrefs.GameWorld));
+      //_chunkProvider.RequestChunk(_x, _y);
+      //_chunkProvider.DoGenerateChunks();
 
       if (_params.Count > 0)
       {
+
         Prefab prefab = new Prefab();
         if (prefab.Load(_params[0]))
         {
+          bool bPhysicsActive = GameManager.bPhysicsActive;
+          GameManager.bPhysicsActive = false;
+
           Vector3i pos = new Vector3i(0, 0, 0);
           int rot = 0;
           int x = (int)sender.position.x - (prefab.size.x / 2);
@@ -63,7 +69,7 @@ namespace BCM.Commands
             // spin the prefab
             for (int r = 0; r < rot % 4; r++)
             {
-              prefab.RotateY(true);
+              prefab.RotateY(false);
             }
 
           }
@@ -77,9 +83,10 @@ namespace BCM.Commands
             y = 255 - prefab.size.y;
           }
 
+          // todo: create an entity observer and spawn prefab once chunks are loaded?
+          
           // todo: define a box first, then spawn prefab centered on middle of the box
           // todo: spawn at position x,z with y=average terrain height?
-          // todo: create an entity observer and spawn prefab once chunks are loaded?
           // todo: allow for partial names for prefab, provide list if more then one result, allow for partial + # from list to specify
           // todo: refresh nearest prefab
 
@@ -99,6 +106,7 @@ namespace BCM.Commands
           {
             for (int cz = -1; cz <= prefab.size.z + 16; cz = cz + 16)
             {
+              // todo: pre generate chunks required
               if (GameManager.Instance.World.IsChunkAreaLoaded(x + cx, y, z + cz))
               {
                 Chunk _chunk = GameManager.Instance.World.GetChunkFromWorldPos(x + cx, y, z + cz) as Chunk;
@@ -109,20 +117,11 @@ namespace BCM.Commands
               }
               else
               {
-                // todo: give warning and exit. require /force option to load prefab with chunks missing
                 SdtdConsole.Instance.Output("Unable to load chunk for prefab @ " + (x + cx) + "," + (z + cz));
               }
             }
           }
 
-          //if (_options.ContainsKey("deco"))
-          //{
-          //  prefab.bAllowTopSoilDecorations = true;
-          //}
-          //if (_options.ContainsKey("nodeco"))
-          //{
-          //  prefab.bAllowTopSoilDecorations = false;
-          //}
           if (_options.ContainsKey("air"))
           {
             prefab.bCopyAirBlocks = true;
@@ -134,28 +133,37 @@ namespace BCM.Commands
 
           // todo: create a copy of the chunks and the bounded dimensions of the prefab size for an undo
           //       should work better than a prefab copy undo as it will preserve block ownership and state?
-          // todo: copy entities to world, add spawner?
           // todo: option to carve terrain where prefab will spawn, maybe reapply decorations?
+          //       insert lot into cell data and regenerate chunk area from world/seed defaults
 
 
-          // todo: debug NRE's
-          //// LOOT PLACEHOLDERS
-          //LootPlaceholderMap lootPlaceholderMap = LootContainer.lootPlaceholderMap;
-          //for (int px = 0; px < prefab.size.x; px++)
-          //{
-          //  for (int py = 0; py < prefab.size.y; py++)
-          //  {
-          //    for (int pz = 0; pz < prefab.size.z; pz++)
-          //    {
-          //      BlockValue bv = prefab.GetBlock(px, py, pz);
-          //      BlockValue bvr = lootPlaceholderMap.Replace(bv, new System.Random());
-          //      if (bv.type != bvr.type)
-          //      {
-          //        prefab.SetBlock(x, y, z, bvr);
-          //      }
-          //    }
-          //  }
-          //}
+          LootPlaceholderMap _map = LootContainer.lootPlaceholderMap;
+          for (int px = 0; px < prefab.size.x; px++)
+          {
+            for (int py = 0; py < prefab.size.y; py++)
+            {
+              for (int pz = 0; pz < prefab.size.z; pz++)
+              {
+                BlockValue bv = prefab.GetBlock(px, py, pz);
+                // todo: copy entities to world, add dynamic spawner?
+                // ENTITIES
+
+
+                // LOOT PLACEHOLDERS
+                // todo: custom block map via configs and select with /map=<mapname>
+                if (bv.type != 0)
+                {
+                  System.Random random = new System.Random(Guid.NewGuid().GetHashCode());
+                  BlockValue bvr = new BlockValue(_map.Replace(bv, random).rawData);
+                  if (bv.type != bvr.type)
+                  {
+                    prefab.SetBlock(px, py, pz, bvr);
+                  }
+                }
+              }
+            }
+          }
+
 
           // SPAWN PREFAB
           Log.Out(Config.ModPrefix + "Spawning prefab " + prefab.filename + " @ " + pos + ", size=" + prefab.size);
@@ -163,12 +171,17 @@ namespace BCM.Commands
           prefab.CopyIntoLocal(GameManager.Instance.World.ChunkCache, pos, true, true);
 
 
-          // todo: check - does this do anything?
           StabilityInitializer _si = new StabilityInitializer(GameManager.Instance.World);
           foreach (Chunk _chunk in chunks.Values)
           {
-            _si.DistributeStability(_chunk);
+            _chunk.ResetStability();
           }
+          foreach (Chunk _chunk in chunks.Values)
+          {
+            _si.DistributeStability(_chunk);
+            _chunk.NeedsRegeneration = true;
+          }
+
 
 
           // todo: confirm - a list of chunks loaded, is it for the server or just that player?
@@ -213,6 +226,7 @@ namespace BCM.Commands
 
           // todo: if not in god mode, and location is not air blocks then teleport to same location with y=-1
 
+          GameManager.bPhysicsActive = bPhysicsActive;
         }
         else
         {
