8b7041b91352389c283285a3837aa97b32e280b2
StompyNZ <russell@codestorm.nz>
2017-01-12 23:58:33 +1300

    Commented Unrequired code

---
+++ BCManager/src/PersistentData/BCMSettings.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace BCM.PersistentData
+{
+  [Serializable]
+  public class BCMSettings
+  {
+    //private bool hideChatCommands;
+    //private string hideChatCommandPrefix;
+
+    //public bool HideChatCommands
+    //{
+    //  get
+    //  {
+    //    return hideChatCommands;
+    //  }
+    //  set
+    //  {
+    //    hideChatCommands = value;
+    //  }
+    //}
+
+    //public string HideChatCommandPrefix
+    //{
+    //  get
+    //  {
+    //    if (hideChatCommandPrefix == null)
+    //    {
+    //      hideChatCommandPrefix = "";
+    //    }
+    //    return hideChatCommandPrefix;
+    //  }
+    //  set
+    //  {
+    //    hideChatCommandPrefix = value;
+    //  }
+    //}
+
+  }
+}
